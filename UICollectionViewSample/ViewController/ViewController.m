6654fe53b81ae32cce7abb49be232939da048f22
velo.yamigiku <velo.yamigiku@gmail.com>
2015-02-14 14:47:49 +0900

    コレクションビューに数字表記のセルを表示するように修正します。

--- UICollectionViewSample/ViewController/ViewController.m
+++ UICollectionViewSample/ViewController/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "NumberCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDelegateFlowLayout>
 
 @end
 
@@ -16,12 +17,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.collectionView registerClass:[NumberCell class]
+            forCellWithReuseIdentifier:@"cell"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+/**
+ *  セルの最大数を返却します。
+ *
+ *  @param collectionView コレクションビュー。
+ *  @param section        セクション。
+ *
+ *  @return セルの最大数。
+ */
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+/**
+ *  コレクションビューに配置するセルを返却します。
+ *
+ *  @param collectionView コレクションビュー。
+ *  @param indexPath      セルの配置番号
+ *
+ *  @return コレクションに配置するセル。
+ */
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NumberCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell"
+                                                                 forIndexPath:indexPath];
+    cell.number = @(indexPath.row + 1);
+    return cell;
+}
+
+/**
+ *  コレクションビューに配置するセルのサイズ（縦、横）を返却します。
+ *
+ *  @param collectionView       コレクションビュー。
+ *  @param collectionViewLayout コレクションビューレイアウト。
+ *  @param indexPath            セルの配置番号。
+ *
+ *  @return コレクションビューに配置するセルのサイズ。
+ */
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [NumberCell cellSizeWithNumber:@(indexPath.row + 1)];
 }
 
 @end
